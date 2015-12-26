3c1c3c546a198b98cc9d2d425b4c8bf1b5af5677
Elena Torro <elenatorro@gmail.com>
2015-12-26 23:15:29 +0100

    last compilation

--- app/app.js
+++ app/app.js
@@ -8,6 +8,10 @@ import dashboard from './modules/dashboard'
 const modules = [uirouter, landing, dashboard]
 
 import FooterMenu from './components/footer/footer.directive'
+import Sidebar from './components/sidebar/sidebar.directive'
+import Icon from './components/icon/icon.directive'
 
 angular.module('app', modules).config(config)
   .directive('footerMenu', FooterMenu)
+  .directive('icon', Icon)
+  .directive('sidebar', Sidebar)
