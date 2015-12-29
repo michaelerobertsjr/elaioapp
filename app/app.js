59d505e30ac634ac578360e35db6e57558bf0dea
Elena Torro <elenatorro@gmail.com>
2015-12-29 19:26:01 +0100

    last compilation

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 import angular from 'angular'
 import uirouter from 'angular-ui-router'
+import angularbootstrap from 'angular-bootstrap'
 import config from './app.config'
 
 import landing from './modules/landing'
 import dashboard from './modules/dashboard'
 
-const modules = [uirouter, landing, dashboard]
+const modules = [uirouter, angularbootstrap, landing, dashboard]
 
 import FooterMenu from './components/footer/footer.directive'
 import Sidebar from './components/sidebar/sidebar.directive'
