dfa9648088de8e32213138d3d0ff013e1195d3b4
Elena Torro <elenatorro@gmail.com>
2016-04-20 23:10:39 +0200

    CHANGED: New modules added

--- app/modules/dashboard/index.js
+++ app/modules/dashboard/index.js
@@ -6,13 +6,23 @@ import routing from './dashboard.routes'
 import DashboardController from './dashboard.controller'
 import CoursesController from './courses/courses.controller'
 import CoursesService from './courses/courses.service'
+import StatementsController from './statements/statements.controller'
+import StatementsService from './statements/statements.service'
 
 import CourseFormDirective from '../../components/course/course.form.directive'
+import CourseFormController from '../../components/course/course.form.controller'
+import UploadfileDirective from '../../components/uploadFile/uploadfile.directive'
+import UploadfileService from '../../components/uploadFile/uploadfile.service'
 
 export default angular.module('app.dashboard', [uirouter, angularbootstrap])
   .config(routing)
   .controller('DashboardController', DashboardController)
   .controller('CoursesController', CoursesController)
+  .controller('StatementsController', StatementsController)
+  .controller('CourseFormController', CourseFormController)
   .directive('courseForm', CourseFormDirective)
+  .directive('uploadfile', UploadfileDirective)
   .service('CoursesService', CoursesService)
+  .service('StatementsService', StatementsService)
+  .service('UploadfileService', UploadfileService)
   .name
